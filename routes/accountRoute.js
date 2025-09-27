6d7034d6d78e2af740b4ec77994c92397235ba4c
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2025-09-27 16:41:34 -0300

    Finishe middle week4

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController"); 
+const regValidade = require("../utilities/account-validation")
 
 // Route to build account login by account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -9,7 +10,19 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build account registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 // Route to process account registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post('/register', 
+    regValidade.registrationRules(), 
+    regValidade.checkRegData, 
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 
 module.exports = router;
