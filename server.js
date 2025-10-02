3be3e9d8d0f261d259788a7979561372370fc3aa
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2025-10-02 13:24:45 -0300

    Authorization and Authentication features added

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities/") 
+const utilities = require("./utilities") 
 const session = require("express-session")
 const pool = require("./database/")
 const accountRoute = require("./routes/accountRoute") 
 const accountController = require("./controllers/accountController")
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 
 /* ***********************
@@ -41,7 +42,6 @@ app.set("layout","./layouts/layout")
   name: 'sessionId',
 }))
 
-
 // Express Messages Middleware
 app.use(require('connect-flash')())
 app.use(function(req, res, next){
@@ -50,7 +50,7 @@ app.use(function(req, res, next){
 })
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(cookieParser())
 
 /* ***********************
  * Routes
