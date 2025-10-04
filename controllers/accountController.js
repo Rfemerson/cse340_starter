20cb2e226d4654733619e3c5f307d0e1b852a635
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2025-10-04 16:28:40 -0300

    Add update account view and route

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -130,6 +130,91 @@ async function buildAccountManagement(req, res, next) {
   })
 }
 
+/* ****************************************
+* Build Account Update View
+* *************************************** */
+async function buildUpdateView(req, res, next) {
+  const account_id = parseInt(req.params.accountId);
+  let nav = await utilities.getNav();
+  const accountData = await accountModel.getAccountById(account_id);
+  res.render("account/update-account", {
+    title: "Update Account Information",
+    nav,
+    errors: null,
+    account_id: accountData.account_id,
+    account_firstname: accountData.account_firstname,
+    account_lastname: accountData.account_lastname,
+    account_email: accountData.account_email,
+  });
+}
+
+/* ****************************************
+* Process Account Information Update
+* *************************************** */
+async function updateAccount(req, res, next) {
+  let nav = await utilities.getNav();
+  const { account_firstname, account_lastname, account_email, account_id } = req.body;
+
+  const updateResult = await accountModel.updateAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_id
+  );
+
+  if (updateResult) {
+    req.flash("notice", "Your account information was successfully updated.");
+    res.redirect("/account/");
+  } else {
+    req.flash("notice", "Sorry, the update failed.");
+    res.status(501).render("account/update-account", {
+      title: "Update Account Information",
+      nav,
+      errors: null,
+      account_id,
+      account_firstname,
+      account_lastname,
+      account_email,
+    });
+  }
+}
+
+/* ****************************************
+* Process Password Update
+* *************************************** */
+async function updatePassword(req, res, next) {
+  let nav = await utilities.getNav()
+  const { account_password, account_id } = req.body
+
+  let hashedPassword
+  try {
+    hashedPassword = await bcrypt.hash(account_password, 10)
+  } catch (error) {
+    req.flash("notice", "Sorry, there was an error processing the password update.")
+    res.status(501).redirect(`/account/update/${account_id}`)
+    return
+  }
+
+  const updateResult = await accountModel.updatePassword(hashedPassword, account_id)
+  
+  if (updateResult) {
+    req.flash("notice", "Your password was successfully updated.")
+    res.redirect("/account/")
+  } else {
+    const accountData = await accountModel.getAccountById(account_id)
+    req.flash("notice", "Sorry, the password update failed.")
+    res.status(501).render(`account/update-account`, {
+      title: "Update Account Information",
+      nav,
+      errors: null,
+      account_id,
+      account_firstname: accountData.account_firstname,
+      account_lastname: accountData.account_lastname,
+      account_email: accountData.account_email,
+    })
+  }
+}
+
 /* **********************************
 * Process logout
 * **********************************/
@@ -138,4 +223,4 @@ async function logout(req, res, next) {
   res.redirect("/")
 }
 
-module.exports = { buildLogin, buildRegister, registerAccount, accountLogin, buildAccountManagement, logout }
+module.exports = { buildLogin, buildRegister, registerAccount, accountLogin, buildAccountManagement, logout, buildUpdateView, updateAccount, updatePassword }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController"); 
-const regValidate = require("../utilities/account-validation")
+const regValidate = require("../utilities/account-validation");
 
 // Route to build account login by account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -16,7 +16,7 @@ router.post(
   regValidate.registrationRules(), 
   regValidate.checkRegData, 
   utilities.handleErrors(accountController.registerAccount)
-)
+);
 
 // Process the login attempt
 router.post(
@@ -24,16 +24,35 @@ router.post(
   regValidate.loginRules(),
   regValidate.checkLoginData,
   utilities.handleErrors(accountController.accountLogin)
-)
+);
 
 // Route to build the account management view
 router.get(
   "/",
-  utilities.checkJWTToken, 
+  utilities.checkLogin, 
   utilities.handleErrors(accountController.buildAccountManagement)
-)
+);
+
+// Route to build the account update view
+router.get("/update/:accountId", utilities.handleErrors(accountController.buildUpdateView));
+
+// Route to process account information update
+router.post(
+  "/update",
+  regValidate.updateAccountRules(),
+  regValidate.checkUpdateData,
+  utilities.handleErrors(accountController.updateAccount)
+);
+
+// Route to process password update
+router.post(
+  "/update-password",
+  regValidate.updatePasswordRules(),
+  regValidate.checkUpdateData,
+  utilities.handleErrors(accountController.updatePassword)
+);
 
 // Route to process logout
-router.get("/logout", utilities.handleErrors(accountController.logout))
+router.get("/logout", utilities.handleErrors(accountController.logout));
 
 module.exports = router;
